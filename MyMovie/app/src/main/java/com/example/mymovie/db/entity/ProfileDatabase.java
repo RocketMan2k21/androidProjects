7b8408e61b396d91d5299b6fd0139025ee282252
RocketMan2k21 <romahaduda@gmail.com>
2024-02-27 12:31:53 +0200

    -

--- MyMovie/app/src/main/java/com/example/mymovie/db/entity/ProfileDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.mymovie.db.entity;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Profile.class}, version = 1)
-public abstract class ProfileDatabase extends RoomDatabase {
-    public abstract ProfileDAO getProfileDAO();
-}
